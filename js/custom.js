cd8a80de1a2ef0da7b479c61ec61693937877520
RezOn_Dev <dev@travelport.ua>
2017-08-14 16:43:55 +0300

    Updates js/custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-    $("<a />").attr("href", "http://rezonuniversal.com").addClass("logo").prependTo($(".book-summary"));
+    var bind = function(){
+        $(".book-summary .logo").remove();
+        $("<a />").attr("href", "http://rezonuniversal.com").addClass("logo").prependTo($(".book-summary"));
+    }
+    gitbook.events.bind("page.change", bind);
+    bind();
 });
